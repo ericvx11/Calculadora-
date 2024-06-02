2acaa0a89b5dd023ecb20aed0cafa311de2de777
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-02 14:13:13 -0600

    añado botones y las cajas de texto

--- Calculadora_Ternimada.cpp
+++ Calculadora_Ternimada.cpp
@@ -2,6 +2,13 @@
 #incluir <cadena>
 #incluir <iostream>
 
+#define ID_BUTTON_SUM 1
+#define ID_BUTTON_RES 2
+#define ID_BUTTON_MUL 3
+#define ID_BUTTON_DIV 4
+#define ID_BUTTON_NOC 5
+
+
 double GetNumberFromEdit(HWND hEdit){
     char buffer[256];
     GetWindowText(hEdit, buffer, 256);
@@ -10,6 +17,19 @@ double GetNumberFromEdit(HWND hEdit){
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	
+	  switch (msg)
+    {
+    case WM_CREATE:
+        hEdit1 = CreateWindow("Edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, 20, 20, 100, 20, hwnd, NULL, NULL, NULL);
+        hEdit2 = CreateWindow("Edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, 130, 20, 100, 20, hwnd, NULL, NULL, NULL);
+        hEditRes = CreateWindow("Edit", "", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_READONLY, 240, 20, 100, 20, hwnd, NULL, NULL, NULL);
+
+        hAdd = CreateWindow("Button", " + ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 20, 60, 60, 20, hwnd, (HMENU)ID_BUTTON_SUM, NULL, NULL);
+        hSub = CreateWindow("Button", " - ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 110, 60, 60, 20, hwnd, (HMENU)ID_BUTTON_RES, NULL, NULL);
+        hMul = CreateWindow("Button", " X ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 60, 60, 20, hwnd, (HMENU)ID_BUTTON_MUL, NULL, NULL);
+        hDiv = CreateWindow("Button", " / ", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 290, 60, 60, 20, hwnd, (HMENU)ID_BUTTON_DIV, NULL, NULL);
+        hClr = CreateWindow("Button", "OtraVez", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 380, 60, 60, 20, hwnd, (HMENU)ID_BUTTON_NOC, NULL, NULL);
+        break;
 	
 }
 
