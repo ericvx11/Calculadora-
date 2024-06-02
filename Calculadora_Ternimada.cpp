d9f7b287e58370c26f3680bc3a8813c0030139ec
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-02 14:11:12 -0600

    cambios

--- Calculadora_Ternimada.cpp
+++ Calculadora_Ternimada.cpp
@@ -2,35 +2,38 @@
 #incluir <cadena>
 #incluir <iostream>
 
-
-doble GetNumberFromEdit(HWND hEdit){
-    búfer de caracteres [256];
-    GetWindowText(hEdit, búfer, 256);
-    devolver atof(buffer);
+double GetNumberFromEdit(HWND hEdit){
+    char buffer[256];
+    GetWindowText(hEdit, buffer, 256);
+    return atof(buffer);
 }
 
-Procedimiento de ventana de devolución de llamada de resultado (HWND hwnd, mensaje UINT, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
+	
+	
 }
 
-// VENTANA GRÁFICA
+
+
+// VENTANA GRAFICA
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow){
     const char CLASS_NAME[] = "Calculadora Proyecto";
-    WNDCLASS baño = {};
-    wc.lpfnWndProc = Procedimiento de ventana;
-    wc.hInstancia = hInst;
-    wc.lpszClassName = CLASE_NOMBRE;
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProcedure;
+    wc.hInstance = hInst;
+    wc.lpszClassName = CLASS_NAME;
 
-    RegistrarClase(&wc);
+    RegisterClass(&wc);
     HWND hwnd = CreateWindow(CLASS_NAME, "Calculadora Proyecto", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 150, NULL, NULL, hInst, NULL);
-    si (hwnd == NULL){
-        devolver 0;
+    if (hwnd == NULL){
+        return 0;
     }
 
-    MostrarVentana(hwnd, nCmdShow);
-    Mensaje MSG = {};
-    mientras (GetMessage(&msg, NULL, 0, 0)){
-        Traducir mensaje(&msg);
-        Mensaje de envío(&msg);
+    ShowWindow(hwnd, nCmdShow);
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
-    devolver 0;
+    return 0;
 }
