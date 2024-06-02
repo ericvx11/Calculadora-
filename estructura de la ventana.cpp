adc5050a5c51ce24483fb32ea753594a4921f638
López Carreto Abril <86798061+abrilloca123@users.noreply.github.com>
2024-06-02 00:02:51 -0600

    Agrego la ventana grafica
    falto la ventana grafica

--- estructura de la ventana.cpp
+++ estructura de la ventana.cpp
@@ -11,3 +11,26 @@ double GetNumberFromEdit(HWND hEdit){
 
 LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 }
+
+// VENTANA GRAFICA
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmdShow){
+    const char CLASS_NAME[] = "Calculadora Proyecto";
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProcedure;
+    wc.hInstance = hInst;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+    HWND hwnd = CreateWindow(CLASS_NAME, "Calculadora Proyecto", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 150, NULL, NULL, hInst, NULL);
+    if (hwnd == NULL){
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)){
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
